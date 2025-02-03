1320d3c3a0ddf06c6317670824ab21a3b756a051
유지원 <jna215843@gmail.com>
2025-02-03 15:10:34 +0900

    tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="text-red-500">Hello World</div>
       </header>
     </div>
   );
