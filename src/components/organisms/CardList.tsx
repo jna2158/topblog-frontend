78fdb8254091e8bbf4f1ac79901e9a716dc09970
유지원 <jna215843@gmail.com>
2025-02-11 15:06:22 +0900

    feat: slider

--- src/components/organisms/CardList.tsx
+++ src/components/organisms/CardList.tsx
@@ -1,12 +1,41 @@
 import React from "react";
+import Slider from "react-slick";
 import Card from "../molecules/Card";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function CardList() {
+  const settings = {
+    dots: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    centerPadding: "25%",
+  };
+
   return (
-    <section className="flex gap-4 mb-28 mt-16 mx-5">
-      <Card />
-      <Card />
-      <Card />
+    <section className="mb-28 mt-16">
+      <Slider {...settings}>
+        <div className="px-4">
+          <Card />
+        </div>
+        <div className="px-4">
+          <Card />
+        </div>
+        <div className="px-4">
+          <Card />
+        </div>
+        <div className="px-4">
+          <Card />
+        </div>
+        <div className="px-4">
+          <Card />
+        </div>
+      </Slider>
     </section>
   );
 }
